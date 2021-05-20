7f23fae3922cc300c2dfed5e246ac5816cd87cde
angelo <aurtula1@gmail.com>
2021-05-20 13:33:53 -0400

    changed provider to google_oauth2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :choir_members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/auth/:provider/callback', to: 'sessions#omniauth'
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
 end
