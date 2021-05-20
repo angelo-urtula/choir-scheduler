ffe8b9a764db896c6cd9c65c58a680a27d2c3075
angelo <aurtula1@gmail.com>
2021-05-20 13:54:41 -0400

    set up new, index, create, edit, show variables

--- app/controllers/choir_members_controller.rb
+++ app/controllers/choir_members_controller.rb
@@ -1,2 +1,31 @@
 class ChoirMembersController < ApplicationController
+
+    def new
+        @choir_member = ChoirMember.new
+    end
+
+    def create
+        @choir_member = ChoirMember.new(choir_member_params)
+        if @choir_member.save
+            redirect_to choir_members_path
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @choir_member = ChoirMember.find(params[:id])
+    end
+
+    def show
+        @choir_member = ChoirMember.find(params[:id])
+    end
+
+    def index
+        @choir_members = ChoirMember.all
+    end
+
+    def choir_member_params
+        params.require(:choir_member).permit(:name, :email, :password, :password_confirmation, :voice, :height, :home_locale, :age, :phone_number, :district)
+      end
 end
