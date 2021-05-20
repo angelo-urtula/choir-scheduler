6aef7c059235b1bd09f458cccd0eb51655f33866
angelo <aurtula1@gmail.com>
2021-05-20 14:13:38 -0400

    redirect to choir members index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
     def omniauth
         user = ChoirMember.create_from_omniauth(auth)
         if user.valid?
-            session[:choir_id]
-            redirect_to new_choir_member_path
+            session[:choir_member_id]
+            redirect_to choir_members_path
         else
             flash[:message] = user.errors.full_messages.join(", ")
             redirect_to choir_members_path
