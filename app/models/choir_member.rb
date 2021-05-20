c541e9222a16bdaa0aff46031dfa4e5792b07f27
angelo <aurtula1@gmail.com>
2021-05-20 14:13:19 -0400

    allowed nil for attributes not given with google

--- app/models/choir_member.rb
+++ app/models/choir_member.rb
@@ -1,11 +1,11 @@
 class ChoirMember < ApplicationRecord
     has_secure_password
     validates :name, presence: true
-    validates :voice, :inclusion => %w(soprano alto tenor base)
-    validates_format_of :phone_number, :with => /[0-9]{10}/
+    validates :voice, :inclusion => %w(soprano alto tenor base), :allow_nil => true
+    validates_format_of :phone_number, :with => /[0-9]{10}/, :allow_nil => true
     validates :email, presence: true, uniqueness: true
-    validates :height, :inclusion => 91..243
-    validates :age, :inclusion => 14..99
+    validates :height, :inclusion => 91..243, :allow_nil => true
+    validates :age, :inclusion => 14..99, :allow_nil => true
 
     def self.create_from_omniauth(auth)
         user = ChoirMember.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
