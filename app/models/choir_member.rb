1634a7b1fabfd170d2828b5fa3fe6efc72ba4045
angelo <aurtula1@gmail.com>
2021-05-20 11:52:56 -0400

    google login creates new choirmember and gives pw

--- app/models/choir_member.rb
+++ app/models/choir_member.rb
@@ -1,2 +1,11 @@
 class ChoirMember < ApplicationRecord
+    has_secure_password
+
+    def self.create_from_omniauth(auth)
+        user = ChoirMember.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.name = auth['info']['name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)
+        end
+    end
 end
