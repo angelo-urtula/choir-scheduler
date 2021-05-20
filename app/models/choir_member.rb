aebca4550f4a3e545f591a849d4a20e44a15d579
angelo <aurtula1@gmail.com>
2021-05-20 13:16:23 -0400

    added validations for choir member model

--- app/models/choir_member.rb
+++ app/models/choir_member.rb
@@ -1,5 +1,11 @@
 class ChoirMember < ApplicationRecord
     has_secure_password
+    validates :name, presence: true
+    validates :voice, presence: true, :inclusion => %w(soprano alto tenor base)
+    validates_format_of :phone_number, :with => /[0-9]{10}/
+    validates :email, uniqueness: true
+    validates :height, presence: true, :inclusion => 91..243
+    validates :age, presence: true, :inclusion => 14..99
 
     def self.create_from_omniauth(auth)
         user = ChoirMember.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
