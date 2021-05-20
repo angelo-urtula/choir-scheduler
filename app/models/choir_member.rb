69a13f4b4fc4c8ffc7c8371079644f895e49e8e0
angelo <aurtula1@gmail.com>
2021-05-20 13:54:06 -0400

    reomved presence validations preventing google

--- app/models/choir_member.rb
+++ app/models/choir_member.rb
@@ -1,11 +1,11 @@
 class ChoirMember < ApplicationRecord
     has_secure_password
     validates :name, presence: true
-    validates :voice, presence: true, :inclusion => %w(soprano alto tenor base)
+    validates :voice, :inclusion => %w(soprano alto tenor base)
     validates_format_of :phone_number, :with => /[0-9]{10}/
-    validates :email, uniqueness: true
-    validates :height, presence: true, :inclusion => 91..243
-    validates :age, presence: true, :inclusion => 14..99
+    validates :email, presence: true, uniqueness: true
+    validates :height, :inclusion => 91..243
+    validates :age, :inclusion => 14..99
 
     def self.create_from_omniauth(auth)
         user = ChoirMember.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
