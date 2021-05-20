bf8fdd3b99a4ae86bcfdadb7012680c55e57f33c
angelo <aurtula1@gmail.com>
2021-05-20 15:28:01 -0400

    added logic for signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,23 @@ class SessionsController < ApplicationController
     def omniauth
         user = ChoirMember.create_from_omniauth(auth)
         if user.valid?
-            session[:choir_member_id]
+            session[:choir_member_id] = user.id
             redirect_to choir_members_path
         else
             flash[:message] = user.errors.full_messages.join(", ")
             redirect_to choir_members_path
+        end
+    end
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:choir_member_id] = @user.id
+        else
+            redirect '/login'
         end
     end
 
