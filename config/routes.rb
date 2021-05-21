d39b61f0e66c7e0d7b33e0952f24c604c3dc09fb
angelo <aurtula1@gmail.com>
2021-05-20 21:19:08 -0400

    added routes for login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root 'sessions#login'
   resources :choir_members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+  get "/login", to: "sessions#login"
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
+  get '/logout', to: "sessions#destroy"
 end
