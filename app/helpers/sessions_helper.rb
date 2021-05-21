48b1ae01b484700b65671144925c2f5d3373711b
angelo <aurtula1@gmail.com>
2021-05-21 10:43:38 -0400

    added helpers for admin login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,19 @@
 module SessionsHelper
 
-    def current_user
+    def current_login
+        if user_logged_in?
         ChoirMember.find(session[:choir_member_id])
+        elsif admin_logged_in?
+        ChoirLeader.find(session[:choir_leader_id])
+        end
     end
 
-    def logged_in?
+    def user_logged_in?
         !!session[:choir_member_id]
+    end
+
+    def admin_logged_in?
+        !!session[:choir_leader_id]
     end
 
     def log_out
