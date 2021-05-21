89099ab3f1aff0669b7b4ae4c910d4933140cb57
angelo <aurtula1@gmail.com>
2021-05-21 10:25:27 -0400

    added possible choir leader login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,12 @@ class SessionsController < ApplicationController
 
     def create
         @user = ChoirMember.find_by(:email => params[:email].downcase)
+        @admin = ChoirLeader.find_by(:email => params[:email].downcase)
         if @user && @user.authenticate(params[:password])
             session[:choir_member_id] = @user.id
+            redirect_to choir_members_path
+        elsif @admin && @admin.authenticate(params[:password])
+            session[:choir_leader_id] = @admin.id
             redirect_to choir_members_path
         else
             flash.now[:danger] = 'Incorrect email and/or password'
