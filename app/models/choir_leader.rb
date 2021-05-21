389ca85bb9c693128181f4586ae5039ce1607f80
angelo <aurtula1@gmail.com>
2021-05-21 10:25:06 -0400

    added validations for choir leader

--- app/models/choir_leader.rb
+++ app/models/choir_leader.rb
@@ -1,4 +1,8 @@
 class ChoirLeader < ApplicationRecord
+    validates :name, presence: true
+    validates :voice, :inclusion => %w(soprano alto tenor base), :allow_nil => true
+    validates_format_of :phone_number, :with => /[0-9]{10}/, :allow_nil => true
+    validates :email, presence: true, uniqueness: true
     has_many :service_times
     has_many :choir_members, through: :service_times
 end
