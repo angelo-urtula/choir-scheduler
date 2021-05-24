b8adce52b48502755a8dd25395ea348a7a41c0a9
angelo <aurtula1@gmail.com>
2021-05-24 16:01:22 -0400

    index for choir leaders

--- app/controllers/choir_leaders_controller.rb
+++ app/controllers/choir_leaders_controller.rb
@@ -2,4 +2,8 @@ class ChoirLeadersController < ApplicationController
     def show
         @choir_leader = ChoirLeader.find(params[:id])
     end
+
+    def index
+        @choir_leaders = ChoirLeader.order('name ASC')
+    end
 end
