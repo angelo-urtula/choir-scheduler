4539df55c960cf38eb4ef10280cfe3313cd16c95
angelo <aurtula1@gmail.com>
2021-05-24 14:36:49 -0400

    refactored servicetime methods

--- app/controllers/service_times_controller.rb
+++ app/controllers/service_times_controller.rb
@@ -12,7 +12,27 @@ class ServiceTimesController < ApplicationController
         redirect_to service_times_path
     end
 
+    def edit
+        find_service
+    end
+
+    def show
+        find_service
+    end
+
+    def update
+        find_service
+        @service_time.update(service_time_params)
+        redirect_to service_times_path
+    end
+
+    private
+
     def service_time_params
         params.require(:service_time).permit(:choir_leader_id, :choir_member_id, :language, :time, :locale, :hymns)
+    end
+
+    def find_service
+        @service_time = ServiceTime.find(params[:id])
     end
 end
