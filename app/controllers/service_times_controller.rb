a3cc8719c5e60ccc2783fe8c8a98fdeed40d0a54
angelo <aurtula1@gmail.com>
2021-05-24 13:56:44 -0400

    methods required to create new service times

--- app/controllers/service_times_controller.rb
+++ app/controllers/service_times_controller.rb
@@ -2,4 +2,17 @@ class ServiceTimesController < ApplicationController
     def index
         @service_times = ServiceTime.all
     end
+
+    def new
+        @service_time = ServiceTime.new
+    end
+    
+    def create
+        ServiceTime.create(service_time_params)
+        redirect_to service_times_path
+    end
+
+    def service_time_params
+        params.require(:service_time).permit(:choir_leader_id, :choir_member_id, :language, :time, :locale, :hymns)
+    end
 end
