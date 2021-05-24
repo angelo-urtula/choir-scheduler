7de2b96eabcea2607030789907e7597386440863
angelo <aurtula1@gmail.com>
2021-05-24 18:27:53 -0400

    added nested route for service times

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'sessions#login'
   resources :choir_members
-  resources :choir_leaders
+  resources :choir_leaders do
+    resources :service_times, only: [:show, :index, :new]
+  end
   resources :service_times
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
