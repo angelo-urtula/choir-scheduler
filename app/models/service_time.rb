8c23367574fbe0a9381d06b761d0543339d2512e
angelo <aurtula1@gmail.com>
2021-05-24 13:57:12 -0400

    made choir member and leader ids optional

--- app/models/service_time.rb
+++ app/models/service_time.rb
@@ -1,5 +1,5 @@
 class ServiceTime < ApplicationRecord
-    belongs_to :choir_member
-    belongs_to :choir_leader
+    belongs_to :choir_member, optional: true
+    belongs_to :choir_leader, optional: true
     scope :todays_services, -> {where(time: DateTime.now.beginning_of_day..DateTime.now.end_of_day)}
 end
