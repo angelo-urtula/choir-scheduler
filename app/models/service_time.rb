a0cfd988a1c93bfc70b49e1b619e42078522aae4
angelo <aurtula1@gmail.com>
2021-05-24 14:28:20 -0400

    added scope method to display future times

--- app/models/service_time.rb
+++ app/models/service_time.rb
@@ -2,4 +2,5 @@ class ServiceTime < ApplicationRecord
     belongs_to :choir_member, optional: true
     belongs_to :choir_leader, optional: true
     scope :todays_services, -> {where(time: DateTime.now.beginning_of_day..DateTime.now.end_of_day)}
+    scope :scheduled_services, -> {where('time > ?', DateTime.now.end_of_day)}
 end
