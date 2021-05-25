544ab54f782856a4e53458b521ca17c0876dca46
angelo <aurtula1@gmail.com>
2021-05-24 23:26:11 -0400

    added edit feature for choir leaders

--- app/controllers/choir_leaders_controller.rb
+++ app/controllers/choir_leaders_controller.rb
@@ -6,4 +6,25 @@ class ChoirLeadersController < ApplicationController
     def index
         @choir_leaders = ChoirLeader.order('name ASC')
     end
+
+    def edit
+        @choir_leader = ChoirLeader.find(params[:id])
+        if current_login == @choir_leader
+            render 'edit'
+        else
+            redirect_to choir_leaders_path
+        end
+    end
+
+    def update
+        @choir_leader = ChoirLeader.find(params[:id])
+        @choir_leader = ChoirLeader.update(choir_leader_params)
+        redirect_to choir_leader_path
+    end
+
+    private
+
+    def choir_leader_params
+        params.require(:choir_leader).permit(:name, :password, :home_locale, :email, :phone_number, :district, :voice)
+    end
 end
