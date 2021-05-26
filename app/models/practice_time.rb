6472d313a7f8f8100df207a605f93dfdbd371a74
angelo <aurtula1@gmail.com>
2021-05-26 18:43:08 -0400

    fixed scope scheduled_practices to not include today

--- app/models/practice_time.rb
+++ app/models/practice_time.rb
@@ -2,7 +2,7 @@ class PracticeTime < ApplicationRecord
     belongs_to :choir_member, optional: true
     belongs_to :choir_leader, optional: true
     scope :todays_practices, -> {where(time: DateTime.now.beginning_of_day..DateTime.now.end_of_day)}
-    scope :scheduled_practices, -> {where('time > ?', DateTime.now.beginning_of_day)}
+    scope :scheduled_practices, -> {where('time > ?', DateTime.now.end_of_day)}
     validates :time, presence: true
     validates :language, presence: true    
 end
