252a8fcd8a1795fd381a1401a3bc3f149d007bbe
angelo <aurtula1@gmail.com>
2021-05-26 15:00:45 -0400

    added language validation for presence

--- app/models/practice_time.rb
+++ app/models/practice_time.rb
@@ -4,4 +4,5 @@ class PracticeTime < ApplicationRecord
     scope :todays_practices, -> {where(time: DateTime.now.beginning_of_day..DateTime.now.end_of_day)}
     scope :scheduled_practices, -> {where('time > ?', DateTime.now.beginning_of_day)}
     validates :time, presence: true
+    validates :language, presence: true    
 end
