40f9fba4c7b1df76f2305d526df41111100cdca0
angelo <aurtula1@gmail.com>
2021-06-02 08:45:46 -0400

    added scope method for reserved practices

--- app/models/practice_time.rb
+++ app/models/practice_time.rb
@@ -4,6 +4,7 @@ class PracticeTime < ApplicationRecord
     scope :todays_practices, -> {where(time: DateTime.now.beginning_of_day..DateTime.now.end_of_day)}
     scope :scheduled_practices, -> {where('time > ?', DateTime.now.end_of_day)}
     scope :available_practices, -> {where(choir_member_id: nil)}
+    scope :reserved_practices, -> {where('choir_member_id != ?', 'nil')}
     validates :time, presence: true
     validates :language, presence: true    
 end
