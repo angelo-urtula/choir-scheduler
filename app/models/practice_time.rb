6c8952899ff30ba2ef3f06bb246d884a5ad3b39c
angelo <aurtula1@gmail.com>
2021-06-02 08:24:26 -0400

    new scope method to show available times

--- app/models/practice_time.rb
+++ app/models/practice_time.rb
@@ -3,6 +3,7 @@ class PracticeTime < ApplicationRecord
     belongs_to :choir_leader, optional: true
     scope :todays_practices, -> {where(time: DateTime.now.beginning_of_day..DateTime.now.end_of_day)}
     scope :scheduled_practices, -> {where('time > ?', DateTime.now.end_of_day)}
+    scope :available_practices, -> {where(choir_member_id: nil)}
     validates :time, presence: true
     validates :language, presence: true    
 end
